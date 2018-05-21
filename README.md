6d8d7c3a1570a9292c3982687bfe0663f3e0f355
Yahe <hello@yahe.sh>
2018-05-21 18:53:59 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Dumpit theme
+The Dumpit theme is a debugging theme for [Urlaub.be](https://github.com/urlaube/urlaube). It just dumps relevant information via `var_dump()` to the output.
+
+## Installation
+Place the folder containing the theme into your themes directory located at `./user/themes/`.
+Finally, add the following line to your configuration file located at `./user/config/config.php` to select the theme:
+```
+Config::THEMENAME("dumpit");
+```
+
+## Configuration
+This theme does not provide any customization.
+
---
+++ theme.php
@@ -0,0 +1,68 @@
+<?php
+
+  /**
+    This is the Dumpit theme.
+
+    This theme just dumps the received content.
+
+    @package urlaube\dumpit
+    @version 0.1a0
+    @author  Yahe <hello@yahe.sh>
+    @since   0.1a0
+  */
+
+  // ===== DO NOT EDIT HERE =====
+
+  // prevent script from getting called directly
+  if (!defined("URLAUBE")) { die(""); }
+
+  if (!class_exists("Dumpit")) {
+    class Dumpit implements Theme {
+
+      public static function theme() {
+        print("METHOD: ");
+        var_dump(Main::METHOD());
+
+        print("PROTOCOL: ");
+        var_dump(Main::PROTOCOL());
+
+        print("HOSTNAME: ");
+        var_dump(Main::HOSTNAME());
+
+        print("PORT: ");
+        var_dump(Main::PORT());
+
+        print("URI: ");
+        var_dump(Main::URI());
+
+        print("ROOTURI: ");
+        var_dump(Main::ROOTURI());
+
+        print("HANDLER: ");
+        var_dump(Handlers::ACTIVE());
+
+        print("PAGEINFO: ");
+        var_dump(Main::PAGEINFO());
+
+        print("PAGENUMBER: ");
+        var_dump(Main::PAGENUMBER());
+
+        print("PAGEMIN: ");
+        var_dump(Main::PAGEMIN());
+
+        print("PAGEMAX: ");
+        var_dump(Main::PAGEMAX());
+
+        print("CONTENT: ");
+        var_dump(Main::CONTENT());
+
+        // we're done
+        return true;
+      }
+
+    }
+
+    // register theme
+    Themes::register("Dumpit", "theme", "dumpit");
+  }
+
